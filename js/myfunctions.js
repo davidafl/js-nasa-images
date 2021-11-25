ff357627fd6d784b3b040fa3257c0056c31cbe4d
David Aflalo <david.aflalo@gmail.com>
2021-11-25 02:03:49 +0200

    search-is-working-no-validation

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -1,11 +1,6 @@
-/* your JS code here */
-
-// this is the correct way to write an Ajax request
-
 // commented out when debugging
 // (function () {
-
-//  check the HTTP response status, returns a promise
+"use strict";
 
 const APIKEY = "dj1uwfwhwqZX3By9FdFIgxlopnNlzmGDD4U3uYeY";
 
@@ -21,19 +16,52 @@ function status(response) {
 function json(response) {
     return response.json()
 }
+let photos = null;
 
-function searchImage(date,mission,camera){
+function searchImage(){
+
+    const mission = document.getElementById("missionid").value;
+    const date = document.getElementById('dateid').value;
+    const camera = document.getElementById('cameraid').value;
 
     fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${mission}/photos?earth_date=${date}&camera=${camera}&api_key=${APIKEY}`)
         .then(status)
         .then(json)
         .then(function (response) {
-            console.log(JSON.stringify(response))
+            console.log(JSON.stringify(response));
+            photos = response.photos;
+            showSearchResults();
+
         })
         .catch(function (error) {
             console.log('Request failed', error);
         });
+}
 
+
+function showSearchResults(){
+    let allcards = "";
+    for (const image of photos) {
+        allcards += toHtmlCard(image);
+    }
+    document.getElementById("cardsid").innerHTML = allcards;
+}
+
+function toHtmlCard(photo) {
+    return `
+            <div class="card" style="width: 18rem;">
+                <img src="${photo.img_src}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Title</h5>
+                    <p class="card-text"> 
+                    ${photo.earth_date} <br/>
+                    ${photo.sol} <br/>  
+                    ${photo.camera.name} <br/>
+                    ${photo.rover.name}</p>  
+                    <a href="#" class="btn btn-primary">Save</a>
+                    <a href="#" class="btn btn-primary">Full Size</a>
+                </div>
+            </div>`
 }
 
 const myarray = {};
@@ -62,12 +90,15 @@ function getData(mission) {
         .catch(function (error) {
             console.log('Request failed', error);
         });
-
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     initMars();
-    document.querySelector("#getdata").addEventListener("click", getData);
+     //document.querySelector("#getdata").addEventListener("click", getData);
+
+    document.getElementById("searchbtn").addEventListener("click", searchImage)
+
+
 }, false);
 
 // })();
